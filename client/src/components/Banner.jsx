2114a95498da013e58fc1de7c2ecb2ff606c13d9
Eli Craig <elidcraig@gmail.com>
2022-09-01 15:08:34 -0400

    logout button functioning

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -4,17 +4,32 @@ import { useAtom } from "jotai";
 import currentUserAtom from "../state/currentUserAtom";
 
 function Banner() {
-  const [currentUser, setCurrentUser] = useAtom(currentUserAtom)
-  console.log(currentUser)
-  
+  const [currentUser, setCurrentUser] = useAtom(currentUserAtom);
+  console.log(currentUser);
+
+  async function handleLogout() {
+    const response = await fetch("/logout", {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      setCurrentUser({});
+    }
+  }
+
   return (
     <div className="banner">
       <Link to="/">
         <div className="banner-logo">MEAL PLANNER</div>
       </Link>
       <div className="banner-button-group">
-        <Link to="/login">Log in</Link>
-        <Link to="/signup">Sign up</Link>
+        {currentUser.id ? (
+          <span onClick={handleLogout}>Log out</span>
+        ) : (
+          <>
+            <Link to="/login">Log in</Link>
+            <Link to="/signup">Sign up</Link>
+          </>
+        )}
       </div>
     </div>
   );
