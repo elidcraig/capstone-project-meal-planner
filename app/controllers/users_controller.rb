fc8f7029be094090d3514faa7e42c5fbd1de0c2e
Eli Craig <elidcraig@gmail.com>
2022-09-01 10:42:06 -0400

    user authentication actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+  def show
+    if session[:user_id]
+      render json: User.find_by(id: session[:user_id])
+    else
+      render json: { errors: ['Problem finding logged in user'] }, status: 401
+    end
+  end
+
+  def create
+    new_user = User.create! user_params
+    session[:user_id] = new_user.id
+    render json: new_user, status: 201
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :username,
+      :password,
+      :password_confirmation,
+      :email
+    )
+  end
+
 end
