4229cadd0aeaeaa56b3c01c5530c041d7fb83ae4
Eli Craig <elidcraig@gmail.com>
2022-09-01 13:54:06 -0400

    site banner added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'
+import Banner from './components/Banner';
 import LoginForm from './pages/LoginForm';
+import SignupForm from './pages/SignupForm';
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <Routes>
         <Route path='/login' element={ <LoginForm/> } />
-        <Route path='/signup' />
+        <Route path='/signup' element={ <SignupForm/> } />
         <Route path='/account' />
       </Routes>
     </div>
---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+function Banner() {
+  return (
+    <div className='banner'>
+      <Link to='/'>
+        <div className='banner-logo'>MEAL PLANNER</div>
+      </Link>
+      <div className='banner-button-group'>
+        <Link to='/login'>Log in</Link>
+        <Link to='/signup'>Sign up</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
